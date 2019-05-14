1bd08ff07aab2385df967453c96aca76976400fe
ProphecyOak <drito610@gmail.com>
2019-05-14 07:23:35 -0400

    More work

--- Main.py
+++ Main.py
@@ -4,12 +4,16 @@ from Consumers import Consumer
 import time
 
 Producer()
+Producer()
+Producer()
+Consumer()
 Consumer()
 Decomposer()
 deadFood = 50
 nutrients = 0
 gen = 0
-print("Gen: " + str(gen),"\nProducers: " + str(Producer.countP),"Consumers: " + str(Consumer.countC),"Decomposers: " + str(Decomposer.countD),"Dead Food: " + str(deadFood))
+print("Gen: " + str(gen),"\nProducers: " + str(Producer.countP),"Consumers: " + str(Consumer.countC),"Decomposers: " + str(Decomposer.countD),
+      "Dead Food: " + str(deadFood),"Produced Food: " + str(Producer.foodP))
 input()
 
 def generationStep():
@@ -17,6 +21,14 @@ def generationStep():
     global deadFood
     global nutrients
     gen += 1
+    
+    if Decomposer.countD <= 0:
+        Decomposer()
+    if Producer.countP <= 0:
+        Producer()
+    if Consumer.countC <= 0:
+        Consumer()
+        Consumer()
     
     p = list(Producer.listP)
     c = list(Consumer.listC)
@@ -28,22 +40,24 @@ def generationStep():
     Decomposer.deadFood = 0
     
     nutrients += Decomposer.foodD
+    for x in c:
+        z = x.eat(deadFood,Producer.foodP)
+        Producer.foodP = int(z[1])
+        deadFood = z[0]
+    for x in d:
+        deadFood = x.eat(deadFood)
     for x in p:
         nutrients = x.eat(nutrients)
     for x in c:
-        x.eat()
+        x.reproduce()
     for x in d:
-        deadFood = x.eat(deadFood)
+        x.reproduce()
+    for x in p:
+        x.reproduce()
         
-    print("Gen: " + str(gen),"\nProducers: " + str(Producer.countP),"Consumers: " + str(Consumer.countC),"Decomposers: " + str(Decomposer.countD),"Dead Food: " + str(deadFood))
+    print("Gen: " + str(gen),"\nProducers: " + str(Producer.countP),"Consumers: " + str(Consumer.countC),"Decomposers: " + str(Decomposer.countD),
+          "Dead Food: " + str(deadFood),"Produced Food: " + str(Producer.foodP))
     input()
-    
-    if Decomposer.countD == 0:
-        Decomposer()
-    if Producer.countP == 0:
-        Producer()
-    if Decomposer.countD == 0:
-        Decomposer()
         
     generationStep()
 generationStep()
