15b7727574883ed2b6e707557213ced8aa2ca372
ProphecyOak <drito610@gmail.com>
2019-05-21 07:12:28 -0400

    Rest of Weekend Work

--- Basic growth3.py
+++ Basic growth3.py
@@ -6,21 +6,23 @@ from functools import partial
 from Consumer2 import Consumer, Producer
 
 
-###Generation Number###
-genNum = 100#input("How many generations?\n>>> ")
-
+genNum = 50
 
 ###Aesthetic Variables###
 proColor = "forest green"
 conColor = "firebrick3"
-resColor = "steelblue"
+conRes = "steelblue"
+proRes = "maroon"
 bacColor = "gainsboro"
-Si = 6
-Sp = 7
+Si = 4
+Sp = 8
 W = 50+genNum*(3*Sp+3)#1400
 H = 600
 Z = 20
-#genNum = 50
+
+
+###Generation Number###
+genNum = 50#input("How many generations?\n>>> ")
 
 
 ###Open Save Data###
@@ -32,14 +34,16 @@ dataList = dataList[:-1]
 
 
 ###Start Values###
-cType = input("K or R?\n>>> ").upper()
+cType = "K0"#input("K or R?\n>>> ").upper()
 gen = 0
-if cType == "K":
+if cType in ["K0","KP"]:
     cPop = [Consumer()]#K
-elif cType == "R":
+elif cType in ["R0","RP"]:
     cPop = [Consumer(3)]#R
 pPop = [Producer()]
-resources = 100
+cResources = 100
+if cType in ["KP","RP"]:
+    pResources = 100
 fruit  = 0
 
 
@@ -62,113 +66,150 @@ C.grid(row=1,column=0,columnspan=30)
 
 
 ###Generation Function###
-def Gene(gen, cPop, pPop, resources, fruit):
-    global proColor, conColor, resColor, bacColor, W, H, Z, Si, Sp, dataList, testNum
+def Gene(gen, cPop, pPop, cResources, fruit,pResources):
+    global proColor, conColor, conRes, proRes, bacColor, W, H, Z, Si, Sp, dataList, testNum
     global B, L, L2, C, cType
     gen += 1
 
 
-    ### K Resources Generation###
-    if cType == "K":
-        resources += 100 - resources + random.randint(0,1)
+    ### K cResources Generation###
+    if cType == "K0":
+        cResources += 100 - cResources + random.randint(0,1)
 
 
     ### R Resource Generation###
-    elif cType == "R":
+    elif cType == "R0":
         if gen%10 == 0:
-            resources += 50 + random.randint(-5,5)
+            cResources += 50 + random.randint(-5,5)
         else:
-            resources += 2 + random.randint(0,1)
-            if resources < 0:
-                resources = 0
+            cResources += 2 + random.randint(0,1)
+            if cResources < 0:
+                cResources = 0
+
+    
+    ###Producer Resources Generation###
+    if cType in ["KP","RP"]:
+        pResources += (gen%10)*10 + random.randint(0,5)
+        for x in pPop:
+            if pResources > 0:
+                cResources += 2
+                pResources -= 1
         
         
     ###Consumer Growth###
-    cPop.sort()
+    cPop.sort(reverse=True)
     for x in cPop:
         x.grow()
-        if x.age >= x.deadAge:
-            cPop.remove(x)
-        elif resources <= 0:
-            cPop.remove(x)
+        if cResources > 0:
+            cResources -= 1
+            if x.age >= x.deadAge:
+                cPop.remove(x)
         else:
-            resources -= 1
+            cPop.remove(x)
+
+
+    ###Producer Growth###
+    if cType in ["KP","RP"]:
+        pPop.sort(reverse=True)
+        for x in pPop:
+            x.grow()
+            if pResources > 0:
+                pResources -= 1
+                if x.age > x.deadAge:
+                    pPop.remove(x)
+            else:
+                pPop.remove(x)
 
 
     ### K Consumer Reproduction###
-    if cType == "K":
+    if cType in ["K0","KP"]:
         for x in cPop:
-            if x.reproductive == 1:
-                if resources > 20 and random.randint(0,1) == 0:
-                    cPop.append(Consumer())
-                    resources -= 5
+            if x.reproductive == 1 and cResources > 20 and random.randint(0,1) == 0:
+                cPop.append(Consumer())
+                if cType in ["K0","KP"]:
+                    cResources -= 5
 
                 
     ### R Consumer Reproduction###
-    elif cType == "R":
+    elif cType in ["R0","RP"]:
         z = 0
-        while resources > 0 and z < len(cPop):
+        while cResources > 0 and z < len(cPop):
             cPop.append(Consumer())
-            resources -= 1
+            cResources -= 1
             z += 1
 
+
+    ###Producer Reproduction###
+    for x in pPop:
+        if x.reproductive == 1 and pResources > 10:
+            for x in range(random.randint(0,10)):
+                pPop.append(Producer())
+                pResources -= 1
+    
         
     ###Data Add###
-    dataList[0].append([int(resources),len(cPop),len(pPop)])
+    dataList[0].append([int(cResources),len(cPop),len(pPop),int(pResources)])
 
 
     ###Graphics Update###
-    L.config(text="R: " + str(int(resources)) + " C: " + str(len(cPop)) + " P: " + str(len(pPop)))
+    L.config(text="pR: " + str(int(pResources)) + " cR: " + str(int(cResources)) + " C: " + str(len(cPop)) + " P: " + str(len(pPop)))
     L2.config(text="Gen: " + str(gen))
-    B.config(command=partial(Gene, gen, cPop, pPop, resources, fruit))
-    C.create_rectangle(gen*(3*Sp+3)+Si+Z,(H-20),gen*(3*Sp+3)+2*Si+Z,(H-20)-int(resources)*3,fill=resColor,outline="")
-    C.create_rectangle(gen*(3*Sp+3)+2*Si+Z,(H-20),gen*(3*Sp+3)+3*Si+Z,(H-20)-len(cPop)*3,fill=conColor,outline="")
-    C.create_rectangle(gen*(3*Sp+3)+3*Si+Z,(H-20),gen*(3*Sp+3)+4*Si+Z,(H-20)-len(pPop)*3,fill=proColor,outline="")
+    B.config(command=partial(Gene, gen, cPop, pPop, cResources, fruit, pResources))
+    C.create_rectangle(gen*(3*Sp+3)+Si+Z,(H-20),gen*(3*Sp+3)+2*Si+Z,(H-20)-(int(pResources)*3),fill=proRes,outline="")
+    C.create_rectangle(gen*(3*Sp+3)+2*Si+Z,(H-20),gen*(3*Sp+3)+3*Si+Z,(H-20)-(int(cResources)*3),fill=conRes,outline="")
+    C.create_rectangle(gen*(3*Sp+3)+3*Si+Z,(H-20),gen*(3*Sp+3)+4*Si+Z,(H-20)-len(cPop)*3,fill=conColor,outline="")
+    C.create_rectangle(gen*(3*Sp+3)+4*Si+Z,(H-20),gen*(3*Sp+3)+5*Si+Z,(H-20)-len(pPop)*3,fill=proColor,outline="")
     if (gen)%5 == 0:
-        C.create_text(gen*(3*Sp+3)+2.5*Si+Z,H-10,text=gen)
+        C.create_text(gen*(3*Sp+3)+3*Si+Z,H-10,text=gen)
     master.update()
     
 
     ###Return###
-    return gen, cPop, pPop, resources, fruit
+    return gen, cPop, pPop, cResources, fruit, pResources
 
 
 ###Test Function###
 def Test():
-    global proColor, conColor, resColor, bacColor, W, H, Z, Si, Sp, dataList, testNum
-    global B, L, L2, C, genNum, gen, cPop, pPop, resources, fruit, cType
+    global proColor, conColor, conRes, proRes, bacColor, W, H, Z, Si, Sp, dataList, testNum
+    global B, L, L2, C, genNum, gen, cPop, pPop, cResources, fruit, cType, pResources
     dataList.insert(0,[])
     C.delete("all")
-    L.config(text="R: " + str(int(resources)) + " C: " + str(len(cPop)) + " P: " + str(len(pPop)))
+    L.config(text="pR: " + str(int(pResources)) + " cR: " + str(int(cResources)) + " C: " + str(len(cPop)) + " P: " + str(len(pPop)))
     L2.config(text="Gen: " + str(gen))
-    B.config(command=partial(Gene, gen, cPop, pPop, resources, fruit))
+    B.config(command=partial(Gene, gen, cPop, pPop, cResources, fruit,pResources))
     for x in range(0,H,50):
         C.create_text(10,H-x*3-20,text=x)
     for x in range(genNum):
-        y = Gene(gen, cPop, pPop, resources, fruit)
+        y = Gene(gen, cPop, pPop, cResources, fruit, pResources)
         gen = y[0]
         cPop = y[1]
         pPop = y[2]
-        resources = y[3]
+        cResources = y[3]
+        pResources = y[5]
         fruit = y[4]
-        if len(cPop) <= 0:
+        if len(cPop) <= 0 or len(pPop) <= 0:
             break
     testNum += 1
 
 
 ###Run Tests###
-testNum = 1#input("How many tests?\n>>> ")
+testNum = int(input("How many tests?\n>>> "))
 for x in range(int(testNum)):
-    Test()
-    gen = 0
-    if cType == "K":
+    cPop = []
+    if cType in ["K0","KP"]:
         cPop = [Consumer()]
-    elif cType == "R":
+    elif cType in ["R0","RP"]:
         cPop = [Consumer(3)]
     pPop = [Producer()]
-    resources = 100
+    cResources = 100
+    pResources = 0
+    if cType in ["KP","RP"]:
+        pResources = 100
     fruit  = 0
-    time.sleep(0.01)
+    gen = 0
+    #time.sleep(0.01)
+    print(x)
+    Test()
 
 
 ###Write Save Data###
--- DataRead.py
+++ DataRead.py
@@ -11,71 +11,79 @@ def Wipe():
 dataList = dataList[:-1]
 proColor = "forest green"
 conColor = "firebrick3"
-resColor = "steelblue"
+conRes = "steelblue"
+proRes = "maroon"
 bacColor = "gainsboro"
 maxGen = 0
 for x in dataList:
     if len(x) > maxGen:
         maxGen = len(x)
 
-newData = [[0,0,0] for x in range(maxGen)]
+newData = [[0,0,0,0] for x in range(maxGen)]
 for x in dataList:
     for y in x:
         i = x.index(y)
-        if newData[i] != [0,0,0]:
+        if newData[i] != [0,0,0,0]:
             avg0 = (y[0]+newData[i][0])/2
             avg1 = (y[1]+newData[i][1])/2
             avg2 = (y[2]+newData[i][2])/2
-            newData[i] = [avg0,avg1,avg2]
+            avg3 = (y[3]+newData[i][3])/2
+            newData[i] = [avg0,avg1,avg2,avg3]
         else:
             newData[i] = y
 for x in range(len(newData)):
     c = newData[x]
-    newData[x] = [int(c[0]),int(c[1]),int(c[2])]
-staDev = [[0,0,0,0,0,0] for x in range(len(newData))]
+    newData[x] = [int(c[0]),int(c[1]),int(c[2]),int(c[3])]
+staDev = [[0,0,0,0,0,0,0,0] for x in range(len(newData))]
 for x in range(len(newData)):
     for y in dataList:
         if len(y) > x:
             staDev[x][0] += (y[x][0]-newData[x][0])**2
-            staDev[x][3] += 1
-            staDev[x][1] += (y[x][1]-newData[x][1])**2
             staDev[x][4] += 1
-            staDev[x][2] += (y[x][2]-newData[x][2])**2
+            staDev[x][1] += (y[x][1]-newData[x][1])**2
             staDev[x][5] += 1
+            staDev[x][2] += (y[x][2]-newData[x][2])**2
+            staDev[x][6] += 1
+            staDev[x][3] += (y[x][3]-newData[x][3])**2
+            staDev[x][7] += 1
         else:
             staDev[x][0] += (0)**2
             staDev[x][1] += (0)**2
             staDev[x][2] += (0)**2
+            staDev[x][3] += (0)**2
 for x in range(len(staDev)):
-    staDev[x][0] = int(math.sqrt(staDev[x][0]/staDev[x][3]))
-    staDev[x][1] = int(math.sqrt(staDev[x][1]/staDev[x][4]))
-    staDev[x][2] = int(math.sqrt(staDev[x][2]/staDev[x][5]))
+    staDev[x][0] = int(math.sqrt(staDev[x][0]/staDev[x][4]))
+    staDev[x][1] = int(math.sqrt(staDev[x][1]/staDev[x][5]))
+    staDev[x][2] = int(math.sqrt(staDev[x][2]/staDev[x][6]))
+    staDev[x][3] = int(math.sqrt(staDev[x][3]/staDev[x][7]))
 
-Avgs = [[],[],[]]
-for x in range(len(newData)):
+Avgs = [[],[],[],[]]
+for x in range(5,len(newData)):
     for y in dataList:
         if len(y) > x:
             Avgs[0].append(y[x][0])
             Avgs[1].append(y[x][1])
             Avgs[2].append(y[x][2])
+            Avgs[3].append(y[x][3])
 Avgs[0] = sum(Avgs[0])/len(Avgs[0])
 Avgs[1] = sum(Avgs[1])/len(Avgs[1])
 Avgs[2] = sum(Avgs[2])/len(Avgs[2])
+Avgs[3] = sum(Avgs[3])/len(Avgs[3])
 print(Avgs)
 #Wipe()
 gen = 0
 W = 2155
 H = 600
 Z = 20
-Si = 6
-Sp = 6
+Si = 8
+Sp = 4
 def gene():
     global gen, newData, W, H, Z, staDev
     L.config(text="R: " + str(int(newData[gen][0])) + " C: " + str(newData[gen][1]) + " P: " + str(newData[gen][2]))
     L2.config(text="Gen: " + str(gen))
-    C.create_rectangle(gen*(3*Si+3)+Sp+Z,(H-20),gen*(3*Si+3)+2*Sp+Z,(H-20)-int(newData[gen][0])*3,fill=resColor,outline="")
-    C.create_line(gen*(3*Si+3)+1.5*Sp+Z,(H-20-int(newData[gen][0])*3),gen*(3*Si+3)+Sp*1.5+Z,(H-20-int(newData[gen][0])*3)-staDev[gen][0],fill=resColor)
-    C.create_line(gen*(3*Si+3)+Sp+Z,(H-20-int(newData[gen][0])*3)-staDev[gen][0],gen*(3*Si+3)+2*Sp+Z,(H-20-int(newData[gen][0])*3)-staDev[gen][0],fill=resColor)
+    C.create_rectangle(gen*(3*Si+3)+Sp+Z,(H-20),gen*(3*Si+3)+2*Sp+Z,(H-20)-int(newData[gen][0])*3,fill=conRes,outline="")
+    C.create_line(gen*(3*Si+3)+1.5*Sp+Z,(H-20-int(newData[gen][0])*3),gen*(3*Si+3)+Sp*1.5+Z,(H-20-int(newData[gen][0])*3)-staDev[gen][0],fill=conRes)
+    C.create_line(gen*(3*Si+3)+Sp+Z,(H-20-int(newData[gen][0])*3)-staDev[gen][0],gen*(3*Si+3)+2*Sp+Z,(H-20-int(newData[gen][0])*3)-staDev[gen][0],fill=conRes)
     
     C.create_rectangle(gen*(3*Si+3)+2*Sp+Z,(H-20),gen*(3*Si+3)+3*Sp+Z,(H-20)-int(newData[gen][1])*3,fill=conColor,outline="")
     C.create_line(gen*(3*Si+3)+2.5*Sp+Z,(H-20-int(newData[gen][1])*3),gen*(3*Si+3)+2.5*Sp+Z,(H-20-int(newData[gen][1])*3)-staDev[gen][1],fill=conColor)
@@ -84,10 +92,14 @@ def gene():
     C.create_rectangle(gen*(3*Si+3)+3*Sp+Z,(H-20),gen*(3*Si+3)+4*Sp+Z,(H-20)-int(newData[gen][2])*3,fill=proColor,outline="")
     C.create_line(gen*(3*Si+3)+3.5*Sp+Z,(H-20-int(newData[gen][2])*3),gen*(3*Si+3)+3.5*Sp+Z,(H-20-int(newData[gen][2])*3)-staDev[gen][2],fill=proColor)
     C.create_line(gen*(3*Si+3)+3*Sp+Z,(H-20-int(newData[gen][2])*3)-staDev[gen][2],gen*(3*Si+3)+4*Sp+Z,(H-20-int(newData[gen][2])*3)-staDev[gen][2],fill=proColor)
+
+    C.create_rectangle(gen*(3*Si+3)+4*Sp+Z,(H-20),gen*(3*Si+3)+5*Sp+Z,(H-20)-int(newData[gen][3])*3,fill=proRes,outline="")
+    C.create_line(gen*(3*Si+3)+4.5*Sp+Z,(H-20-int(newData[gen][3])*3),gen*(3*Si+3)+4.5*Sp+Z,(H-20-int(newData[gen][3])*3)-staDev[gen][3],fill=proRes)
+    C.create_line(gen*(3*Si+3)+4*Sp+Z,(H-20-int(newData[gen][3])*3)-staDev[gen][3],gen*(3*Si+3)+5*Sp+Z,(H-20-int(newData[gen][3])*3)-staDev[gen][3],fill=proRes)
     
     gen += 1
     if (gen)%5 == 0:
-        C.create_text(gen*(3*Si+3)+2.5*Sp+Z,H-10,text=gen)
+        C.create_text(gen*(3*Si+3)+3*Sp+Z,H-10,text=gen)
     master.update()
 master = tkinter.Tk()
 master.geometry("%dx%d+%d+%d" % (W,H+30,0,0))
@@ -101,12 +113,14 @@ C = tkinter.Canvas(master,width=W,height=H,bg=bacColor)
 C.grid(row=1,column=0,columnspan=30)
 for x in range(0,H,50):
     C.create_text(10,H-x*3-20,text=x)
-C.create_rectangle(gen*(3*Si+3)+Sp+Z,(H-20),gen*(3*Si+3)+2*Sp+Z,(H-20)-newData[gen][0]*3,fill=resColor,outline=resColor)
-C.create_rectangle(gen*(3*Si+3)+2*Sp+Z,(H-20),gen*(3*Si+3)+3*Sp+Z,(H-20)-newData[gen][1]*3,fill=conColor,outline=conColor)
-C.create_rectangle(gen*(3*Si+3)+3*Sp+Z,(H-20),gen*(3*Si+3)+4*Sp+Z,(H-20)-newData[gen][2]*3,fill=proColor,outline=proColor)
-C.create_text(gen*(3*Si+3)+2.5*Sp+Z,H-10,text=gen)
+C.create_rectangle(gen*(3*Si+3)+Sp+Z,(H-20),gen*(3*Si+3)+2*Sp+Z,(H-20)-newData[gen][0]*3,fill=conRes,outline="")
+C.create_rectangle(gen*(3*Si+3)+2*Sp+Z,(H-20),gen*(3*Si+3)+3*Sp+Z,(H-20)-newData[gen][1]*3,fill=conColor,outline="")
+C.create_rectangle(gen*(3*Si+3)+3*Sp+Z,(H-20),gen*(3*Si+3)+4*Sp+Z,(H-20)-newData[gen][2]*3,fill=proColor,outline="")
+C.create_rectangle(gen*(3*Si+3)+4*Sp+Z,(H-20),gen*(3*Si+3)+4*Sp+Z,(H-20)-newData[gen][2]*3,fill=proRes,outline="")
+C.create_text(gen*(3*Si+3)+3*Sp+Z,H-10,text=gen)
 for x in range(len(newData)):
     gene()
-C.create_line(Z,Avgs[0],W,Avgs[0],fill=resColor)
-C.create_line(Z,Avgs[1],W,Avgs[1],fill=conColor)
-C.create_line(Z,Avgs[2],W,Avgs[2],fill=proColor)
+C.create_line(5*(3*Si+3)+Sp+Z,(H-20)-Avgs[0]*3,W,(H-20)-Avgs[0]*3,fill=conRes)
+C.create_line(5*(3*Si+3)+Sp+Z,(H-20)-Avgs[1]*3,W,(H-20)-Avgs[1]*3,fill=conColor)
+C.create_line(5*(3*Si+3)+Sp+Z,(H-20)-Avgs[2]*3,W,(H-20)-Avgs[2]*3,fill=proColor)
+C.create_line(5*(3*Si+3)+Sp+Z,(H-20)-Avgs[3]*3,W,(H-20)-Avgs[3]*3,fill=proRes)
